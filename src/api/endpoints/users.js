3c54fe94efe57d16b9fd52b0ddf5cae8dd204c91
trebolsoft <trebolsoftv1@gmail.com>
2026-01-02 13:48:20 -0500

    Sincronizando cambios locales

--- src/api/endpoints/users.js
+++ src/api/endpoints/users.js
@@ -2,31 +2,31 @@
 import api from '../axios';
 
 export const getUsers = async () => {
-  const response = await api.get('/api/v1/users/');
+  const response = await api.get('/users/');
   return response.data;
 };
 
 export const createUser = async (userData) => {
-  const response = await api.post('/api/v1/users/', userData);
+  const response = await api.post('/users/', userData);
   return response.data;
 };
 
 export const getUser = async (userId) => {
-  const response = await api.get(`/api/v1/users/${userId}`);
+  const response = await api.get(`/users/${userId}`);
   return response.data;
 };
 
 export const getMe = async () => {
-  const response = await api.get('/api/v1/users/me');
+  const response = await api.get('/users/me');
   return response.data;
 };
 
 export const updateUser = async (userId, userData) => {
-  const response = await api.put(`/api/v1/users/${userId}`, userData);
+  const response = await api.put(`/users/${userId}`, userData);
   return response.data;
 };
 
 export const deleteUser = async (userId) => {
-  const response = await api.delete(`/api/v1/users/${userId}`);
+  const response = await api.delete(`/users/${userId}`);
   return response.data;
 };
